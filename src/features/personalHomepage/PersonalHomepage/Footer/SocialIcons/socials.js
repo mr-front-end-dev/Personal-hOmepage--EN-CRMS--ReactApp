7ac8975cc9560c7c4967d0f6cdc963fdf715ca94
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-24 18:32:59 +0100

    icons added

--- src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
+++ src/features/personalHomepage/PersonalHomepage/Footer/SocialIcons/socials.js
@@ -1,8 +1,8 @@
 import { styleIcon } from "./styled";
 import { ReactComponent as GitHubIcon } from "./icons/github.svg";
-// import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
+import { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
 import { ReactComponent as LinkedinIcon } from "./icons/linkedin.svg";
-// import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
+import { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
 
 export const socials = [
   {
@@ -10,19 +10,19 @@ export const socials = [
     url: "https://github.com/Mr-Front-End-dev",
     Icon: styleIcon(GitHubIcon),
   },
-  // {
-  //   name: "Facebook",
-  //   url: "https://facebook.com",
-  //   Icon: styleIcon(FacebookIcon),
-  // },
+  {
+    name: "Facebook",
+    url: "https://facebook.com",
+    Icon: styleIcon(FacebookIcon),
+  },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/nikita-rysiev-a4b7921b4",
     Icon: styleIcon(LinkedinIcon),
   },
-  // {
-  //   name: "Instagram",
-  //   url: "https://instagram.com",
-  //   Icon: styleIcon(InstagramIcon),
-  // },
+  {
+    name: "Instagram",
+    url: "https://instagram.com",
+    Icon: styleIcon(InstagramIcon),
+  },
 ]
