57330671a7440249af38afcc36363c36af6f1b5d
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-03-01 10:30:32 +0100

    &nbsp; to Summary added

--- src/features/personalHomepage/PersonalHomepage/MainInformation/index.js
+++ src/features/personalHomepage/PersonalHomepage/MainInformation/index.js
@@ -1,19 +1,15 @@
 import nikitaRysievProfile from "./profile.jpg";
-// import Projects from "../Projects"
-// import Clock from "../Clock";
 import { Avatar, StyledButtonLink, ButtonIcon, Name, Summary, ThisIs, Wrapper } from "./styled"; 
 import { email } from "../email";
 
 export const MainInformation = () => (
   <Wrapper>
-    {/* <Clock /> */}
-    {/* <Projects /> */}
     <Avatar src={nikitaRysievProfile} alt="Nikita Rysiev" />
     <div>
       <ThisIs>This is</ThisIs>
       <Name>Nikita Rysiev</Name>
       <Summary contentEditable>
-        I am a passionate 👶 Junior Front-End (💻) Developer in love with React, and Junior Manual and Automation Software Tester currently 🔍 I am looking for new job opportunities.
+        I am a passionate 👶 Junior Front-End (💻) Developer in love with React, and Junior Manual and Automation Software Tester currently&nbsp;🔍 I&nbsp;am looking&nbsp;for new&nbsp;job opportunities.
       </Summary>
       <StyledButtonLink href={`mailto:${email}`} title={email}>
         <ButtonIcon />
